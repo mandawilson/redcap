54b8573247cb35177118480db04efd9789005af4
Wilson <wilsonm2@plvibioportal1.mskcc.org>
2018-05-11 20:04:06 -0400

    finds all differences does not update data

--- upload_csv_to_redcap_v2.py
+++ upload_csv_to_redcap_v2.py
@@ -1,8 +1,6 @@
-# TODO trim fields from input file
-# TODO we found modified value, but not new/deleted fields
-#   make sure we get everything from `diff modified_darwin_eight_batches.csv /data/redcap_scratch/darwin_eight_batches.csv`
-# TODO then delete any patient with modifed/deleted data, and add all their data back
-# TODO then add any new fields (do not add if already added to deleted patient)
+# TODO test that if we delete an entire record we find that
+# TODO actually update redcap
+# TODO make sure we get everything from `diff modified_darwin_eight_batches.csv /data/redcap_scratch/darwin_eight_batches.csv`
 # WARNING this is ignoring *_complete fields TODO add back
 # TODO check headers are the same
 import redcap
@@ -18,26 +16,28 @@ verbose = True
 csv_filename = "modified_darwin_eight_batches.csv"
 primary_key = "patient_id"
 
-instruments_to_record_id_to_key_to_new_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
-
+record_id_to_key_to_new_dict = defaultdict(lambda: defaultdict(dict))
+record_id_to_key_to_old_dict = defaultdict(lambda: defaultdict(dict))
 # TODO need the field mapping, and it needs to define the record_id
 # read in CSV and store instrument -> record id -> rows
+# these are the 'new' records
 with open(csv_filename, 'rb') as csv_file:
   csvreader = csv.DictReader(csv_file, delimiter=',', quotechar='"')
   for row in csvreader:
-    # TODO Redcap is trimming the fields (I think), we should too
+    # Redcap is trimming the fields (I think), we should too
+    row = { k:v.strip() for k, v in row.iteritems()}
     instrument_name = row["redcap_repeat_instrument"] if row["redcap_repeat_instrument"] else primary_key 
     # TODO this should not be included in the CSV file
     del row["redcap_repeat_instance"] # this might be different, so remove it from the record
     # sort values based on sorted keys, do not just sort values -- fields could be in different order but result in the same
     # (even though it is unlikely) -- do the same below too
-    instruments_to_record_id_to_key_to_new_dict[instrument_name][row[primary_key]]["".join([v for k,v in sorted(row.items())])] = row
+    #print "LOG:", row
+    record_id_to_key_to_new_dict[row[primary_key]]["".join([v for k,v in sorted(row.items())])] = row
 
-# download all data for project, one instrument at a time (to save some memory)
+# download all data for project, one instrument at a time
+# these are the 'old' records
 instruments = redcap.get_instruments(pid, verbose)
 for instrument in instruments:
-  record_ids_to_key_to_old_dict = defaultdict(lambda: defaultdict(dict))
-  max_repeat_instance = None
   if verbose:
     print "LOG: downloading rows for %s" % (instrument["instrument_name"])
   old_rows = redcap.get_records(pid, instrument["instrument_name"], primary_key, verbose)
@@ -45,32 +45,62 @@ for instrument in instruments:
     #print "LOG: downloaded row", row
     if "redcap_repeat_instance" in row:
       if row["redcap_repeat_instance"] == "":
-	if verbose:
+        if verbose:
           print "LOG: skipping row '%s' because it is a redcap_repeat_instance row with no redcap_repeat_instance, it is just an empty row with only patient id set" % (row)
         continue
-      max_repeat_instance = max(max_repeat_instance, row["redcap_repeat_instance"])
       del row["redcap_repeat_instance"]
     # TODO remove this, we should probably include this column in the input file
     #   or somehow set it to be an optional column
-    if "redcap_repeat_instrument" in row:
-      del row["%s_complete" % (row["redcap_repeat_instrument"])]
+    complete_field_name = "%s_complete" % (instrument["instrument_name"])
+    if complete_field_name in row:
+      #print "LOG: deleting '%s' from row" % (complete_field_name)
+      del row[complete_field_name]
 
     key = "".join([str(v) for k,v in sorted(row.items())])
     #print "LOG: saving key '%s', row '%s'" % (key, row)
-    record_ids_to_key_to_old_dict[row[primary_key]][key] = row
+    exit
+    record_id_to_key_to_old_dict[row[primary_key]][key] = row
 
-  # TODO make sure headers are the same
-  # now loop through each new record id and figure out if anything changed
-  print "instruments_to_record_id_to_key_to_new_dict[instrument['instrument_name']] size:", len(instruments_to_record_id_to_key_to_new_dict[instrument["instrument_name"]])
-  for record_id, key_to_new_dict in instruments_to_record_id_to_key_to_new_dict[instrument["instrument_name"]].iteritems():
-    for key in key_to_new_dict:
-      if verbose:
-        print "LOG: checking record '%s' with key '%s'" % (record_id, key)
-	#for old_key in record_ids_to_key_to_old_dict[record_id].iterkeys():
-	#  print "LOG: old_key = '%s', key is type '%s'" % (old_key, type(old_key))
-      if key not in record_ids_to_key_to_old_dict[record_id]:
-        print "Did NOT find '%s' in old records" % (key)
-	print "Example:", record_ids_to_key_to_old_dict[record_id].iterkeys().next() 
-      #else:
-      #  print "Found '%s' in old records" % (key)
-    #sys.exit()
+# what records are new?
+# what records have been deleted?
+new_record_id_set = set(record_id_to_key_to_new_dict.keys())
+old_record_id_set = set(record_id_to_key_to_old_dict.keys())
+deleted_record_ids = old_record_id_set - new_record_id_set # in old but not in new
+added_record_ids = new_record_id_set - old_record_id_set # in new but not in old
+records_in_both_sets = old_record_id_set & new_record_id_set # in new and in old
+changed_record_ids = set([])
+
+# for the records that were in redcap before and we still want, has any data changed?
+# TODO make sure headers are the same
+for record_id in records_in_both_sets:
+  old_data_set = set(record_id_to_key_to_new_dict[record_id].keys())
+  new_data_set = set(record_id_to_key_to_old_dict[record_id].keys())
+  # get data in old set or new set, but NOT IN BOTH, if there is at least one record difference, 
+  # this record needs to be deleted and re-added
+  different_data = old_data_set ^ new_data_set
+  if len(different_data) > 0:
+    if verbose:
+      for data in different_data:
+        print "LOG: '%s' is different between the old and new records" % (data)
+        if data in old_data_set:
+          print "LOG: the above was found in the old data set dict is", record_id_to_key_to_old_dict[record_id][data]
+        if data in new_data_set:
+          print "LOG: the above was found in the new data set dict is", record_id_to_key_to_new_dict[record_id][data]
+
+    changed_record_ids.add(record_id)
+
+print "LOG: %d records in old set" % (len(old_record_id_set))
+print "LOG: %d records in new set" % (len(new_record_id_set))
+print "LOG: %d added records" % (len(added_record_ids))
+print "LOG: %d deleted records" % (len(deleted_record_ids))
+print "LOG: %d records changed" % (len(changed_record_ids))
+
+# TODO delete data for modified records and deleted records
+record_ids_to_delete = changed_record_ids | deleted_record_ids
+record_ids_to_add = changed_record_ids | added_record_ids
+for record_id in record_ids_to_delete:
+  if verbose:
+    print "LOG: delete record '%s'" % (record_id)
+for record_id in record_ids_to_add:
+  if verbose:
+    print "LOG: add record '%s'" % (record_id)
